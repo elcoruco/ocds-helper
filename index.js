509b39ee5bb427d990174b31971730cfaaab5ce8
el coruco <el.coruco@gmail.com>
2020-10-17 22:46:31 -0500

    add accesor

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const release  = require("./schemas/release.json");
 const releaseP = require("./schemas/release-package.json");
 const recordP  = require("./schemas/record-package.json");
-console.log(release, releaseP, recordP, "alv");
+
+const RELEASE  = "release";
+const RELEASEP = "release package";
+const RECORDP  = "record Package";
+
 exports.ocdsSchemas = {
   release,
   releaseP,
   recordP
 }
+
+exports.jsonType = file => {
+  if(typeof file !== "object" || file === null) return null;
+  if(file.releases) return RELEASEP;
+  if(file.records) return RECORDP;
+  if(file.ocid) return RELEASE;
+  return null;
+}
+
+exports.createOCDSHelper = ocds => {
+  console.log("schemas:", this.ocdsSchemas);
+  return {
+    ocds,
+    type : this.jsonType(ocds),
+    schemas : this.ocdsSchemas
+  }
+}
+
--- test/index.js
+++ test/index.js
@@ -1,3 +1,16 @@
 const readOCDS = require("../index");
+const axios    = require("axios");
 
-console.log("wtf:", readOCDS);
+//const helper = readOCDS.createOCDSHelper({ocid : 12});
+
+// test secop release 1
+axios.get("/ocds/secop-release_1.json").then(res => {
+  const helper = readOCDS.createOCDSHelper(res.data)
+  console.log("secop:", helper);
+});
+
+// test inai record package 1
+axios.get("/ocds/inai-record-package_1.json").then(res => {
+  const helper = readOCDS.createOCDSHelper(res.data)
+  console.log("inai:", helper);
+});
